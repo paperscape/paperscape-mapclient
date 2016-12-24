d9a86b76234bd161ad8d304f5e62ad110825d824
Rob Knegjens <rknegjens@gmail.com>
2016-12-24 14:20:39 +0100

    Switching to jquery 3.1.1 from 1.8.3, involved upating jquery.ui too. Also updated require.js. Also removed redundant dependence on a second jquery.js file in build scripts.

--- js/app-build.js
+++ js/app-build.js
@@ -1,12 +1,24 @@
 ({
     baseUrl: ".",
     paths: {
-        "jquery": "lib/jquery",
+        "jquery": "lib/jquery-3.1.1.min",
         "jquery.mousewheel": "lib/jquery.mousewheel",
         "jquery.autocomplete": "lib/jquery-ui-autocomplete.min",
         "jquery.autocomplete.html": "lib/jquery-ui-autocomplete.html",
         "requireLib": "lib/require"
     },
+    shim: {
+      "jquery.mousewheel": {
+        deps: ["jquery"]
+      },
+      "jquery.autocomplete": {
+        deps: ["jquery"]
+      },
+      "jquery.autocomplete.html": {
+        deps: ["jquery", "jquery.autocomplete"]
+      }
+    },
+    wrapShim: true,
     name: "app/config",
     include: "requireLib",
     out: "app-build/pscp.js"
